d29bc49db2ed09d42495a0f5d5b915a67728c719
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2024-10-14 11:21:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,20 @@
 
+import mongoose from "mongoose"
+
+/**
+ * 
+ * @param {String} URI 
+ * @returns void
+ */
+const connectionToDB = async (URI) => {
+    mongoose.connect(process.env.MONGO_URI)
+    const db = mongoose.connection
+    db.on("open", () => {
+        console.log("DB Connected..")
+    })
+
+    db.on("error", () => {
+        console.log("Connection failed.")
+    })
+}
+export default connectionToDB;
